6d71cb76da2e2bb391cc58f39975cf35685edd17
Estevao <paraltae@gmail.com>
2021-01-07 14:42:21 +0000

    ficheiros

---
+++ jurojs.js
@@ -0,0 +1,8 @@
+var ValFinal;
+var ValInicial;
+var tempo;
+var Valjuro; //tem de ser decimal//
+var ValPerJuro; //numero de vezes que o juro é aplicado por ano//
+var ValIncremento; //Incremento opcional//
+var ValPerIncremento; //periodicidade do incremento//
+
