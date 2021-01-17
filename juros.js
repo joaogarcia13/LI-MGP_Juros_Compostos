1a9e286ae12000a61a3272122bf91530337fe2e2
GoncaloGoncalves33 <goncalo.goncalves9@gmail.com>
2021-01-17 15:38:40 +0000

    puxar

--- juros.js
+++ juros.js
@@ -509,10 +509,10 @@ function ExportarExcel() {
 }
 //exportação PDF
 function ExportarPDF() {
-    var doc = new jsPDF("p", "mm", "a4")
-    var h1 = document.querySelector('#resetTabela')
-
-    doc.fromHTML(h1, 50, 15)
-    doc.setFontSize(160);
+    var doc = new jsPDF()
+    doc.autoTable({html:'#resetTabela'})
     doc.save("JurosCompostos.PDF")
+
+    var canvas = document.querySelector('#chart');
+    doc.addImage(canvas, 'JPEG', 10, 10, 280, 150 );
 }
